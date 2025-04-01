0c7887feb267595388bb1463cdc8c1500e1d2817
Dacus <Dacus@Test.me>
2025-04-01 16:21:26 +0100

    readme format change

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ Collection of files for the noobspec whoop frame
 ## Images
 
 ### Dacus
-![elise.png](https://github.com/dargust/NOOBSpec/blob/main/Dacus/elise.png?raw=true)
-![horus.png](https://github.com/dargust/NOOBSpec/blob/main/Dacus/horus.png?raw=true)
+![elise.png](https://github.com/dargust/NOOBSpec/blob/main/Dacus/elise.png?raw=true) ![horus.png](https://github.com/dargust/NOOBSpec/blob/main/Dacus/horus.png?raw=true) 
 ### Iron-Ryan
-![runcam nano 3 45°.png](https://github.com/dargust/NOOBSpec/blob/main/Iron-Ryan/runcam%20nano%203%2045°.png?raw=true)
+![runcam nano 3 45°.png](https://github.com/dargust/NOOBSpec/blob/main/Iron-Ryan/runcam%20nano%203%2045°.png?raw=true) 
--- generate_readme.py
+++ generate_readme.py
@@ -16,16 +16,20 @@ with open(readme_file, "w") as f:
     f.write("# NOOBSpec canopy repository\n")
     f.write("Collection of files for the noobspec whoop frame\n")
     f.write("\n\n")
-    f.write("## Images\n\n")
+    f.write("## Images\n")
     last_dir = ""
+    line_position = 1
+    max_images_per_line = 2
     for dir_file in png_files:
         #if the directory is not the same as the last directory, write the directory name as header
         if dir_file[0] != last_dir:
             last_dir = dir_file[0]
-            f.write(f"### {dir_file[0]}\n")
+            f.write(f"\n### {dir_file[0]}\n")
         #add image to the readme file
         url_dir = dir_file[0].replace(" ", "%20")
         url_file = dir_file[1].replace(" ", "%20")
         url_encoded_file = f"https://github.com/dargust/NOOBSpec/blob/main/{url_dir}/{url_file}?raw=true"
         #write the image to the readme file with a link to the file in github
-        f.write(f"![{dir_file[1]}]({url_encoded_file})\n")
+        new_line = "\n" if line_position == max_images_per_line else " "
+        line_position = (line_position + 1) % (max_images_per_line)
+        f.write(f"![{dir_file[1]}]({url_encoded_file}){new_line}")
